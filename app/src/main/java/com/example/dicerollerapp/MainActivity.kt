73eea84b03dc4f4cb89e128078f210022d7c9afb
Daniel Vedamuthu <daniel.pravin@gmail.com>
2025-05-01 21:40:50 -0500

    Added a button

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.dicerollerapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dicerollerapp.ui.theme.DiceRollerAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             DiceRollerAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    DiceRoller(
                         name = "Daniel",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +40,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun DiceRoller(name: String, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Column (
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(text = "Roll it $name!")
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DiceRollerAppTheme {
-        Greeting("Daniel")
+        Button (onClick = {
+            Toast.makeText(context, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+        }) {
+            Text("Roll Dice...")
+        }
     }
 }
